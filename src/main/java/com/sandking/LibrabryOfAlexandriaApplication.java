d847076099a205f32e24c99ba95b1b8f6d4fc74b
Sandk1nGG <eazy97889@gmail.com>
2023-06-27 23:59:29 +0700

    test

---
+++ src/main/java/com/sandking/LibrabryOfAlexandriaApplication.java
@@ -0,0 +1,13 @@
+package com.sandking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrabryOfAlexandriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrabryOfAlexandriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandking/LibrabryOfAlexandriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrabryOfAlexandriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
